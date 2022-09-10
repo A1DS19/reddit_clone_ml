cbbd2f900468b8fa6c400cdcad07ad04750ca4e4
Jose Padilla <jp06171@gmail.com>
2022-09-10 00:14:17 -0600

    server script update/ENV update client

--- client/config/axios.ts
+++ client/config/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_SERVER_URL
+      : process.env.NEXT_PUBLIC_PROD_SERVER_URL,
   withCredentials: true,
 });
