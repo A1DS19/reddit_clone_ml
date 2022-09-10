54770dc005288fd11ddd252651a6280c6be0d66a
Jose Padilla <jp06171@gmail.com>
2022-09-09 22:48:27 -0600

    server updatre

--- server/src/config/TypeOrmConfig.ts
+++ server/src/config/TypeOrmConfig.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Comment } from 'src/entities/comment.entity';
-import { Community } from 'src/entities/community.entity';
-import { Post } from 'src/entities/post.entity';
-import { Reply } from 'src/entities/reply.entity';
-import { User } from 'src/entities/user.entity';
-import { Vote } from 'src/entities/vote.entity';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { IEnvConfiguration } from './envConfiguration';
 
@@ -21,7 +15,8 @@ export class TypeOrmConfig {
       username: this.configService.get('DB_USERNAME'),
       password: this.configService.get('DB_PASSWORD'),
       database: this.configService.get('DB_NAME'),
-      entities: [User, Post, Comment, Reply, Vote, Community],
+      entities: ['dist/entities/**/*.entity.js'],
+      migrations: ['dist/migrations/**/*.js'],
       synchronize: true,
       logging: 'all',
       logger: 'advanced-console',
@@ -32,14 +27,15 @@ export class TypeOrmConfig {
     return {
       type: this.configService.get('DB_TYPE'),
       url: process.env.DATABASE_URL,
-      entities: ['./dist/src/**/*.entity.js'],
-      migrations: ['./dist/src/migrations/*.js'],
+      entities: ['dist/entities/**/*.entity.js'],
+      migrations: ['dist/migrations/**/*.js'],
+      synchronize: true,
+      logging: 'all',
+      logger: 'advanced-console',
+      migrationsRun: false,
       ssl: {
         rejectUnauthorized: false,
       },
-      synchronize: false,
-      logging: 'all',
-      logger: 'advanced-console',
     };
   }
 
